eeac4e2b3ac0db4249334d1326af54f3239db229
banana <ahmadjd94@gmail.com>
2024-04-26 18:42:16 +0300

    debugging

--- app/repositories/products.py
+++ app/repositories/products.py
@@ -1,13 +1,12 @@
-from sqlalchemy.orm import Session
-from fastapi import HTTPException
-from app.schemas.products import ProductsIn
-from app import models
-def add_land_product(land_info: ProductsIn ,db: Session):
-    land_info = db.query(models.Product).filter(models.Product.os_listing_id == land_info.os_listing_id).first()
-    if land_info:
-        raise HTTPException(status_code=400, detail="land info already added")
-    land_obj = models.Product(**land_info)
-    db.add(land_obj)
-    db.commit()
-    db.refresh()
-    return land_obj
+# from fastapi import HTTPException
+# from app.schemas.products import ProductsIn
+# from app import models
+# def add_land_product(land_info: ProductsIn ,db: Session):
+#     land_info = db.query(models.Product).filter(models.Product.os_listing_id == land_info.os_listing_id).first()
+#     if land_info:
+#         raise HTTPException(status_code=400, detail="land info already added")
+#     land_obj = models.Product(**land_info)
+#     db.add(land_obj)
+#     db.commit()
+#     db.refresh()
+#     return land_obj
