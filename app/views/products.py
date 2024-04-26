e2b3c355e670b94d81c98649e2bea16f97cded43
banana <ahmadjd94@gmail.com>
2024-04-26 18:43:05 +0300

    debugging

--- app/views/products.py
+++ app/views/products.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, Depends
 from fastapi.responses import JSONResponse
 from app.schemas.products import ProductsIn
-from app.repositories.products import add_land_product
+# from app.repositories.products import add_land_product
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.db import get_session
 
@@ -11,5 +11,5 @@ router = APIRouter()
 
 @router.post("")
 async def add_product(req_payload: ProductsIn, db: AsyncSession = Depends(get_session)):
-    data = await add_land_product(req_payload, db)
-    return JSONResponse(status_code=status.HTTP_200_OK, content=data)
+    # data = await add_land_product(req_payload, db)
+    return JSONResponse(status_code=status.HTTP_200_OK, content={})
