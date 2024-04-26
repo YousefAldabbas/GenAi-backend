a75b0a3d8848afe8b4798185e3151d6d1eebc8dd
banana <ahmadjd94@gmail.com>
2024-04-26 18:44:20 +0300

    debugging

--- app/views/products.py
+++ app/views/products.py
@@ -1,15 +1,15 @@
-from fastapi import APIRouter, status, Depends
-from fastapi.responses import JSONResponse
-from app.schemas.products import ProductsIn
-# from app.repositories.products import add_land_product
-from sqlalchemy.ext.asyncio import AsyncSession
-from core.db import get_session
-
-
-router = APIRouter()
-
-
-@router.post("")
-async def add_product(req_payload: ProductsIn, db: AsyncSession = Depends(get_session)):
-    # data = await add_land_product(req_payload, db)
-    return JSONResponse(status_code=status.HTTP_200_OK, content={})
+# from fastapi import APIRouter, status, Depends
+# from fastapi.responses import JSONResponse
+# from app.schemas.products import ProductsIn
+# # from app.repositories.products import add_land_product
+# # from sqlalchemy.ext.asyncio import AsyncSession
+# from core.db import get_session
+#
+#
+# router = APIRouter()
+#
+#
+# @router.post("")
+# async def add_product(req_payload: ProductsIn, db: AsyncSession = Depends(get_session)):
+#     # data = await add_land_product(req_payload, db)
+#     return JSONResponse(status_code=status.HTTP_200_OK, content={})
--- app/views/users.py
+++ app/views/users.py
@@ -1,30 +1,30 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from app import dependencies as deps
-from app import models
-from app.repositories import users as usr_repo
-from app.schemas import users as usr_schema
-from core.db import get_session
-
-router = APIRouter()
-
-
-@router.post("/users")
-async def create_user(
-    payload: usr_schema.UserIn, db: AsyncSession = Depends(get_session)
-):
-    """
-    Create a new user
-    """
-    return http_handler(data=await usr_repo.create_user(db, payload), status_code=201)
-
-
-@router.get("/users/profile")
-async def get_user_profile(user: models.User = Depends(deps.get_current_user)):
-    """
-
-    Get user profile
-    """
-
-    return http_handler(data=usr_repo.get_user_profile(user), status_code=200)
+# from fastapi import APIRouter, Depends
+# from sqlalchemy.ext.asyncio import AsyncSession
+#
+# from app import dependencies as deps
+# from app import models
+# from app.repositories import users as usr_repo
+# from app.schemas import users as usr_schema
+# from core.db import get_session
+#
+# router = APIRouter()
+#
+#
+# @router.post("/users")
+# async def create_user(
+#     payload: usr_schema.UserIn, db: AsyncSession = Depends(get_session)
+# ):
+#     """
+#     Create a new user
+#     """
+#     return http_handler(data=await usr_repo.create_user(db, payload), status_code=201)
+#
+#
+# @router.get("/users/profile")
+# async def get_user_profile(user: models.User = Depends(deps.get_current_user)):
+#     """
+#
+#     Get user profile
+#     """
+#
+#     return http_handler(data=usr_repo.get_user_profile(user), status_code=200)
